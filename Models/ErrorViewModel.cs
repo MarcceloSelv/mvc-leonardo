db162d2fb97342ff604c73d4d470608806d6a041
marccelo@signainfo.com.br <marccelo@signainfo.com.br>
2021-11-26 16:28:02 -0300

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Leonardo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
